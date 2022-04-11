82e1306aae4fd07866510601d4fbf7c0d35b3233
Sara Trapp <38925365+Aradhela@users.noreply.github.com>
2022-04-11 22:34:18 +0200

    Update script.js
    zmiana w pliku js (rozszerzenie do milisekund, sekund, minut oraz godzin)

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
 
 let countTime;
-let minutes = 0;
+
+let miliseconds = 0;
 let seconds = 0;
+let minutes = 0;
 let hours = 0;
+
 
 let timesArr = [];
 
@@ -36,59 +39,225 @@ const handleStart = () => {
     clearInterval(countTime);
 
     countTime = setInterval(() => {
-        if (seconds < 9) {
-            seconds++;
+        if (miliseconds < 9) {
+            miliseconds++;
 
-            if(minutes <= 9){
-                stopwatch.textContent = `${hours}:0${minutes}:0${seconds}`
+            if(seconds <= 9){
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:0${seconds}:0${miliseconds}`
+                }
+
+                else if((minutes > 9 && minutes <= 59)){
+
+                    stopwatch.textContent = `${hours}:${minutes}:0${seconds}:0${miliseconds}`
+
+                }
+
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:0${seconds}:0${miliseconds}`
+ 
+                }
             }
+            
+            else if((seconds > 9 && seconds <= 59)){
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:${seconds}:0${miliseconds}`
+                }
+
+                else if((minutes > 9 && minutes <= 59)){
+
+                    stopwatch.textContent = `${hours}:${minutes}:${seconds}:0${miliseconds}`
+
+                }
+
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:${seconds}:0${miliseconds}`
+ 
+                }
+
+
+
+            }
+
+            
+            else{
+             minutes++;
+             seconds = 0;
+
+             if(minutes <= 9){
+                stopwatch.textContent = `${hours}:0${minutes}:00:0${miliseconds}`
+
+            }
+
             else if((minutes > 9 && minutes <= 59)){
-                stopwatch.textContent = `${hours}:${minutes}:0${seconds}`
+
+                stopwatch.textContent = `${hours}:${minutes}:00:0${miliseconds}`
+
             }
 
             else{
-             hours++;
-             minutes = 0;
-             stopwatch.textContent = `${hours}:00:0${seconds}`
+                minutes = 0;
+                hours++;
+                stopwatch.textContent = `${hours}:00:00:0${miliseconds}`
+
+            }
+
+
 
             }
 
 
         }
         
-        else if (seconds >= 9 && seconds < 59) {
-            seconds++;
+        else if (miliseconds >= 9 && miliseconds < 99) {
+            miliseconds++;
 
-            if(minutes <= 9){
-                stopwatch.textContent = `${hours}:0${minutes}:${seconds}`
+            if(seconds <= 9){
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:0${seconds}:${miliseconds}`
+                }
+
+                else if((minutes > 9 && minutes <= 59)){
+
+                    stopwatch.textContent = `${hours}:${minutes}:0${seconds}:${miliseconds}`
+
+                }
+
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:0${seconds}:${miliseconds}`
+ 
+                }
+
+
+
             }
-            else if((minutes > 9 && minutes <= 59)){
-                stopwatch.textContent = `${hours}:${minutes}:${seconds}`
+            
+            else if((seconds > 9 && seconds <= 59)){
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:${seconds}:${miliseconds}`
+                }
+
+                else if((minutes > 9 && minutes <= 59)){
+
+                    stopwatch.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`
+
+                }
+
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:${seconds}:${miliseconds}`
+ 
+                }
+
             }
+            
             else{
-                hours++;
-                minutes = 0;
-                stopwatch.textContent = `${hours}:00:${seconds}`
+                minutes++;
+                seconds = 0;
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:00:${miliseconds}`
+    
+                }
+    
+                else if((minutes > 9 && minutes <= 59)){
+    
+                    stopwatch.textContent = `${hours}:${minutes}:00:${miliseconds}`
+    
+                }
+    
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:00:${miliseconds}`
+    
+                }
+
  
             }
 
         }
         
         else {
-            minutes++;
-            seconds = 0
+            seconds++;
+            miliseconds = 0
 
-            if(minutes <= 9){
-                stopwatch.textContent = `${hours}:0${minutes}:00`
+            if(seconds <= 9){
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:0${seconds}:00`
+                }
+
+                else if((minutes > 9 && minutes <= 59)){
+
+                    stopwatch.textContent = `${hours}:${minutes}:0${seconds}:00`
+
+                }
+
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:0${seconds}:00`
+ 
+                }
+
             }
-            else if((minutes > 9 && minutes <= 59)){
-                stopwatch.textContent = `${hours}:${minutes}:00`
+            
+            else if((seconds > 9 && seconds <= 59)){
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:${seconds}:00`
+                }
+
+                else if((minutes > 9 && minutes <= 59)){
+
+                    stopwatch.textContent = `${hours}:${minutes}:${seconds}:00`
+
+                }
+
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:${seconds}:00`
+ 
+                }
+
             }
 
+            
             else{
-                hours++;
-                minutes = 0;
-                stopwatch.textContent = `${hours}:${minutes}:00`
+                minutes++;
+                seconds = 0;
+
+                if(minutes <= 9){
+                    stopwatch.textContent = `${hours}:0${minutes}:00:00`
+    
+                }
+    
+                else if((minutes > 9 && minutes <= 59)){
+    
+                    stopwatch.textContent = `${hours}:${minutes}:00:00`
+    
+                }
+    
+                else{
+                    minutes = 0;
+                    hours++;
+                    stopwatch.textContent = `${hours}:00:00:00`
+    
+                }
+
 
             }
 
@@ -98,14 +267,14 @@ const handleStart = () => {
 
 
 
-    }, 100);
+    }, 10);
 }
 
 const handleStop = () => {
 
     time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`
 
-    if (stopwatch.textContent !== '00:00:00') {
+    if (stopwatch.textContent !== '0:00:00:00') {
         time.style.visibility = 'visible';
         timesArr.push(stopwatch.textContent)
     };
@@ -125,8 +294,9 @@ const handleReset = () => {
 
 const clearStuff = () => {
     clearInterval(countTime);
-    stopwatch.textContent = '0:00:00';
+    stopwatch.textContent = '0:00:00:00';
     timeList.textContent = '';
+    miliseconds = 0;
     seconds = 0;
     minutes = 0;
     hours = 0;
